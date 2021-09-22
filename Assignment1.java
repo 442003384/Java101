ce3a43d78e848e87e066eab63c26d72d8abca7e2
waleed alshehri <91115666+442003384@users.noreply.github.com>
2021-09-23 00:00:10 +0300

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -4,14 +4,13 @@ class Main {
   public static void main(String [] args) {
     Scanner myObj = new Scanner(System.in);
     float number1;
-    //هنا أنت مكرر عملية التعريف للأسكانر مرتين ، تقدر تستخدم نفس السطر ٥ في كل مرة
-     Scanner mylbj = new Scanner(System.in);
+    
     float number2;
     // Enter username and press Enter
     System.out.println("Enter a number");
     number1 = myObj.nextFloat(); 
     System.out.println("Enter a number"); 
-    number2 = mylbj.nextFloat();
+    number2 = myObj.nextFloat();
        
     System.out.println(number1 + number2);
     System.out.println(number1 - number2);
@@ -23,7 +22,6 @@ class Main {
     System.out.println(number1 < number2);
     System.out.println(number1 <= number2);
     System.out.println(number1 != number2);
-    // هل هذه العملية عملية مساواة أم عملية اسناد قيمة المتغير ٢ إلى المتغير ١
     System.out.println(number1 = number2);
   }
 }
