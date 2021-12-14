006613e45a4ffbb24186086ff3adab5f6c7219ae
waleed alshehri <91115666+442003384@users.noreply.github.com>
2021-12-14 18:51:02 +0300

    Create readWordByWord.java

---
+++ readWordByWord.java
@@ -0,0 +1,10 @@
+public class readWordByWord.java {
+        public static void main(String[] args) {
+                String xxx = "waleed\nalshrhri";
+
+                System.out.println(xxx);
+
+
+
+        }
+}
