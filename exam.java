661cb633550e18e55a7e8107edb43e7c35465113
waleed alshehri <91115666+442003384@users.noreply.github.com>
2021-12-21 08:17:03 +0300

    Create exam.java

---
+++ exam.java
@@ -0,0 +1,14 @@
+public class Main {
+        public static void main(String[] args) {
+
+               for (int counter =1; counter<=100; counter++){
+                    if(counter%2==1)
+                        System.out.println(counter+"%2 is ood");
+                    else {
+                        System.out.println(counter+"%2 is even");
+                    }
+
+               }
+
+        }
+}
